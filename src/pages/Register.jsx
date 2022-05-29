d295e5227f7fe4ff035f27652fadbbeb34fae5a7
saurabhraj123 <me4saurabh4u@gmail.com>
2022-05-29 18:56:19 +0530

    Registration

---
+++ src/pages/Register.jsx
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-image: url('https://images.pexels.com/photos/2229490/pexels-photo-2229490.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+`;
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  /* height: 100vh; */
+  background: white;
+  width: 40%;
+  /* height: 50vh; */
+  padding: 15px;
+  box-shadow: 1px 1px #e7faff;
+`;
+
+const Title = styled.h1`
+  align-self: flex-start;
+  font-size: 25px;
+  margin-bottom: 15px;
+  margin-top: 10px;
+`;
+
+const InputArea = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const Input = styled.input`
+  width: 43%;
+  padding: 7px 10px;
+  margin: 10px 6px;
+  font-size: 17px;
+  font-weight: 400;
+`;
+const Info = styled.span`
+  margin-top: 10px;
+  font-size: 15px;
+  font-weight: 300;
+`;
+const Button = styled.button`
+  align-self: flex-start;
+  margin-top: 15px;
+  padding: 15px 80px;
+  border: none;
+  color: white;
+  background: teal;
+  font-weight: 500;
+  cursor: pointer;
+  margin-bottom: 10px;
+`;
+
+const Bold = styled.span`
+  font-weight: bold;
+`;
+
+const Register = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Title>CREATE AN ACCOUNT</Title>
+        <InputArea>
+          <Input placeholder="first name" type="text" />
+          <Input placeholder="last name" type="text" />
+          <Input placeholder="username" type="text" />
+          <Input placeholder="email" type="email" />
+          <Input placeholder="password" type="password" />
+          <Input placeholder="confirm password" type="password" />
+        </InputArea>
+        <Info>
+          By creating an account, I consent to the processing of my personal
+          data in accordance with <Bold>PRIVACY POLICY.</Bold>
+        </Info>
+        <Button>CREATE</Button>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Register;
